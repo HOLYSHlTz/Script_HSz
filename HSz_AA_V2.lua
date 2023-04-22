1b3c4a9ee3d88bf0886a52a22defb32416310aa0
siradaniy <83637146+siradaniy@users.noreply.github.com>
2023-04-22 17:18:47 +0700

    Update HSz_AA_V2.lua

--- HSz_AA_V2.lua
+++ HSz_AA_V2.lua
@@ -1559,6 +1559,8 @@ function savepos(UnitPos, a,a2,a3,a4,a5,a6)
         updatepos("7ds", UnitPos, a,a2,a3,a4,a5,a6)
     elseif game.Workspace._map:FindFirstChild("bridge nocollide") then
         updatepos("mha", UnitPos, a,a2,a3,a4,a5,a6)
+    elseif game.Workspace._map:FindFirstChild("benches and barrels")
+        updatepos("entertain", UnitPos, a,a2,a3,a4,a5,a6)
     end
     warn("savepos")
 end
@@ -3964,6 +3966,8 @@ coroutine.resume(coroutine.create(function()
                 PlaceUnitsTEST("7ds")
             elseif game.Workspace._map:FindFirstChild("bridge nocollide") then
                 PlaceUnitsTEST("mha")
+            elseif game.Workspace._map:FindFirstChild("benches and barrels") then
+                PlaceUnitsTEST("entertain")
             end
         end
     end
@@ -4017,6 +4021,8 @@ coroutine.resume(coroutine.create(function()
                 PlaceUnits("7ds")
             elseif game.Workspace._map:FindFirstChild("bridge nocollide") then
                 PlaceUnits("mha")
+            elseif game.Workspace._map:FindFirstChild("benches and barrels") then
+                PlaceUnits("entertain")
             end
         end
     end

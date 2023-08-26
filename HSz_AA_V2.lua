b5a4f07ef62e66bccf7c24ab2710bbbc26d57c8e
siradaniy <83637146+siradaniy@users.noreply.github.com>
2023-08-26 21:03:35 +0700

    Update HSz_AA_V2.lua

--- HSz_AA_V2.lua
+++ HSz_AA_V2.lua
@@ -1,5 +1,5 @@
 --updatefix
-local version = "16.0.0-1b"
+local version = "16.0.0-1c"
 
 ---// Loading Section \\---
 repeat  task.wait() until game:IsLoaded()
@@ -197,7 +197,8 @@ function webhook()
         --Difficulty
         MapDiff2 = game:GetService("Players").LocalPlayer.PlayerGui.ResultsUI.Holder.Difficulty.Text
         MapDiff3 = MapDiff2
-        if GetLevelData.name == "Summer Hunt" then MapDiff3 = " [ Default ] Is a Portal " end
+        --if GetLevelData.name == "Summer Hunt" then MapDiff3 = " [ Default ] Is a Portal " end
+        if poratltierS ~= " Not a Portal " then MapDiff3 = " Hard " end
         -------------------------------
 
         cwaves = game:GetService("Players").LocalPlayer.PlayerGui.ResultsUI.Holder.Middle.WavesCompleted.Text

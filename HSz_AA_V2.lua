04996f6a42014e2545f8132189c20905a6f791fc
siradaniy <83637146+siradaniy@users.noreply.github.com>
2023-08-28 03:43:38 +0700

    Update HSz_AA_V2.lua

--- HSz_AA_V2.lua
+++ HSz_AA_V2.lua
@@ -199,6 +199,7 @@ function webhook()
         MapDiff3 = MapDiff2
         --if GetLevelData.name == "Summer Hunt" then MapDiff3 = " [ Default ] Is a Portal " end
         if poratltierS ~= " Not a Portal " then MapDiff3 = " Hard " end
+        if poratltierS == " Not a Portal " then MapDiff3 = MapDiff2 end
         -------------------------------
 
         cwaves = game:GetService("Players").LocalPlayer.PlayerGui.ResultsUI.Holder.Middle.WavesCompleted.Text

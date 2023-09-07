63beb2c845de0f426718304ff4757dc4487c288f
siradaniy <83637146+siradaniy@users.noreply.github.com>
2023-09-07 19:03:49 +0700

    Update HSz_AA_V2.lua

--- HSz_AA_V2.lua
+++ HSz_AA_V2.lua
@@ -21,7 +21,7 @@ else
 end
 
 --Version_UI
-UPDUI2 = game:GetService('HttpService'):JSONDecode(readfile("UPD_name.txt"))
+UPDUI2 = game:GetService('HttpService'):JSONDecode(readfile("UPD_name.json"))
 local version = tostring(UPDUI2)
 --local version = "16.0.0-1xx"
 ------------------------------
